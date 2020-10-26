b99a208f88e917d88f430f6e33a4ce251d94199e
ALBERTO <albertitovalls97@gmail.com>
2020-10-26 18:52:40 +0100

    Mostrar recyclerview dispositivo

--- app/src/main/java/com/example/androidappgestionbasura/utility/AppConf.java
+++ app/src/main/java/com/example/androidappgestionbasura/utility/AppConf.java
@@ -1,13 +1,21 @@
 package com.example.androidappgestionbasura.utility;
 
 import android.app.Application;
-import android.os.SystemClock;
 
+import com.example.androidappgestionbasura.model.Dispositivos;
+import com.example.androidappgestionbasura.model.DispositivosLista;
 import com.example.androidappgestionbasura.model.Usuario;
+import com.example.androidappgestionbasura.presentacion.adapters.AdaptadorDispositivos;
 
 public class AppConf extends Application {
 
     private Usuario usuario;
+    public Dispositivos dispositivos = new DispositivosLista();
+    @Override public void onCreate() {
+        super.onCreate();
+    }
+
+    public AdaptadorDispositivos adaptador = new AdaptadorDispositivos(dispositivos);
 
 
     public Usuario getUsuario() {
