a418d6a678f375b2090dc573a606e25aacb33db6
Ruben <rubenpardocasanova@gmail.com>
2021-01-19 16:24:06 +0100

    Añadir boton crear cuenta con google

--- app/src/main/java/com/example/androidappgestionbasura/presentacion/AuthActivity.java
+++ app/src/main/java/com/example/androidappgestionbasura/presentacion/AuthActivity.java
@@ -11,6 +11,7 @@ import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.view.WindowManager;
